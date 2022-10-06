b631a1e2658987adbbe53515ee8e37e867c6e98f
mousa4007 <solayman6236@gmail.com>
2022-10-06 10:45:24 +0300

    new updates

--- app/Http/Controllers/Api/PurchaseProductController.php
+++ app/Http/Controllers/Api/PurchaseProductController.php
@@ -225,7 +225,7 @@ class PurchaseProductController extends Controller
         $user = $request->user();
         $product = Product::find($request->product_id);
 
-        dd($product->id);
+      
 
 
 
