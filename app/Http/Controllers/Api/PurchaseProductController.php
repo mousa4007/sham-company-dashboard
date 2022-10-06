2790e46575b859502c8211bf6e8ba0a6e8980e73
mousa4007 <solayman6236@gmail.com>
2022-10-06 10:29:17 +0300

    new updates

--- app/Http/Controllers/Api/PurchaseProductController.php
+++ app/Http/Controllers/Api/PurchaseProductController.php
@@ -225,7 +225,7 @@ class PurchaseProductController extends Controller
         $user = $request->user();
         $product = Product::find($request->product_id);
 
-        dd(  $user ->ougtgoingBalance + $product->sell_price);
+        dd($user->ougtgoingBalance + $product->sell_price);
 
 
             Sale::create([
