17fcc741de8bae5dbcc3ae0a37675e3afd6e8683
mousa4007 <solayman6236@gmail.com>
2022-10-11 21:15:44 +0300

    new updates

--- resources/views/livewire/discounts/product_discount_group_modal.blade.php
+++ resources/views/livewire/discounts/product_discount_group_modal.blade.php
@@ -23,7 +23,7 @@
                                             <span class="input-group-text" id="basic-addon1"><i
                                                     class="bi bi-search"></i></span>
                                             <input type="text" class="form-control" aria-label="Recipient's username"
-                                                aria-describedby="button-addon2" wire:model='searchTerm'>
+                                                aria-describedby="button-addon2" >
                                             <button class="btn btn-outline-secondary" type="button"
                                                 id="button-addon2">بحث</button>
                                         </div>
