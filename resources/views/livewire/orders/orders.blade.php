1bf1af12d0f30d79f45bbb7f0b4dbdaa5354bc3d
mousa4007 <solayman6236@gmail.com>
2022-10-11 21:25:01 +0300

    new updates

--- resources/views/livewire/orders/orders.blade.php
+++ resources/views/livewire/orders/orders.blade.php
@@ -63,7 +63,7 @@
                                         <td>{{ $order->price }}</td>
                                         <td>{{ $order->product_id}}</td>
                                         <td>{{ $order->app_user_id }}</td>
-                                      <td>{{ \App\Models\Category::find($order->product_id) }}</td>
+                                      <td>{{ \App\Models\Category::find($order->product_id) != null ? \App\Models\Category::find($order->product_id)->name :'' }}</td>
                                         <td>{{ \App\Models\AppUser::find($order->app_user_id) }}</td>
                                         <td>{{ $order->created_at }}</td>
                                     </tr>
