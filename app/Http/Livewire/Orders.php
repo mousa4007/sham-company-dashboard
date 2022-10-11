fbbcc486847ee226d4887dcc75d8556f670c2a39
mousa4007 <solayman6236@gmail.com>
2022-10-11 23:40:35 +0300

    new updates

--- app/Http/Livewire/Orders.php
+++ app/Http/Livewire/Orders.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Category;
 use App\Models\Order;
+use App\Models\Product;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -32,7 +34,9 @@ class Orders extends Component
     public function render()
     {
         return view('livewire.orders.orders',[
-            'orders' => $this->orders
+            'orders' => $this->orders,
+            'products' => Product::all(),
+            'categories' => Category::all(),
         ]);
     }
 
