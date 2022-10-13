3e2b58f3b46b65997c4b88469b8dd2e2a5227364
mousa4007 <solayman6236@gmail.com>
2022-10-13 13:45:59 +0300

    new updates

--- app/Http/Controllers/Api/PurchaseProductController.php
+++ app/Http/Controllers/Api/PurchaseProductController.php
@@ -256,8 +256,9 @@ class PurchaseProductController extends Controller
                         $profit = $product->sell_price - $exception->first()->price;
                     }
 
-                    dd('here exceptions');
-                } else {
+                    // dd('here exceptions');
+                }
+                 else {
 
                     $profit = abs($product->sell_price * Discount::find($user->discount)->percentage / 100);
 
@@ -285,6 +286,7 @@ class PurchaseProductController extends Controller
                     $user->update(['total_profits' => $user->total_profits + $profit]);
                 }
             } else {
+                dd('here');
                $order =  Order::create([
                     'app_user_id' => $user->id,
                     'product_id' => $product->id,
@@ -297,6 +299,8 @@ class PurchaseProductController extends Controller
                 dd($order);
 
             }
+
+            dd('here before update balance');
 
             $user->update([
                 'balance' => $user->balance - $product->sell_price,
