cba4de1a25a8b22369efa063d9881470836329ed
mousa4007 <solayman6236@gmail.com>
2022-10-14 23:34:24 +0300

    new updates

--- resources/views/livewire/discounts/product_discount_modal.blade.php
+++ resources/views/livewire/discounts/product_discount_modal.blade.php
@@ -2,7 +2,7 @@
     <!-- Update Product Discount -->
     <div wire:ignore.self class="modal fade" id="updateProductDiscount" tabindex="-1" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
-        <div class="modal-dialog modal-xl">
+        <div class="modal-dialog modal-xl modal-dialog-scrollable">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">إدارة نسب
