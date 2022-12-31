04372e74474b99e14918ad04f8a52c9dc7384070
mousa4007 <solayman6236@gmail.com>
2022-12-31 17:11:20 +0300

    new

--- resources/views/livewire/orders/orders.blade.php
+++ resources/views/livewire/orders/orders.blade.php
@@ -88,14 +88,14 @@
                     <table class="table table-bordered m-1 bg-secondary">
                         <thead>
                             <th class="text-white">مجموع السعر</th>
-                            <td class="text-white">{{$orders_in_last_day->sum('price')}} $</td>
+                            <td class="text-white">{{number_format($orders_in_last_day->sum('price'),2)}} $</td>
 
                         </thead>
                     </table>
                     <table class="table table-bordered m-1 bg-secondary">
                         <thead>
                             <th class="text-white">مجموع مربح الوكيل</th>
-                            <td class="text-white">{{$orders_in_last_day->sum('profit')}} $</td>
+                            <td class="text-white">{{number_format($orders_in_last_day->sum('profit'),2)}} $</td>
                         </thead>
                     </table>
                    </div>
